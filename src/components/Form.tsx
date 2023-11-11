b65a5e06148b33a070a838680e4d52200675eabe
Gautam Bansal <g.bansal@edexa.team>
2023-11-11 13:15:57 +0300

    feat : adding wallet starknetKit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { connect, disconnect } from "starknetkit";
+import abi from "./middleware/abi.json";
+
+
 import Deposit from "./Deposit";
 import Withdraw from "./Withdraw";
+
+const contractAddress =  "0x056839737baa24d9a9648bde92e3f6b97f777327e12bd0afb4ed0b4478093509";
 
 export default function Form() {
 	const [form, setForm] = useState<number>(1);
 	const [isConnect, setIsConnect] = useState<boolean>(false);
 
+	
+	const [provider, setProvider] = useState();
+	const [address, setAddress] = useState();
+	const [connection, setConnection] = useState();
+	
+	console.log(provider, "provider");
+	console.log(address, "address");
+	console.log(connection, "connection");
+	
 	const handleFormChange = (val: number) => {
 		setForm(val);
+		if(val == 1){
+
+		}else{
+
+		}
 	};
+
+
+	const deposit = async () => {
+
+		let core = new starknet.Contract(abi, address, provider)
+
+
+	}
 
 	const walletStatusChange = (value: boolean) => {
 		setIsConnect(value);
 	};
 
-	const walletHandle = () => {
-		// call wallet status change
-
-		walletStatusChange(true);
+	const walletHandle = async () => {
+		const connection = await connect();
+		if (connection && connection.isConnected) {
+			setConnection(connection);
+			setProvider(connection.account);
+			setAddress(connection.selectedAddress);
+			walletStatusChange(true);
+		}
 	};
+
+	useEffect(() => {
+		const connectToStarknet = async () => {
+			const connection = await connect({ modalMode: "neverAsk" });
+			if (connection && connection.isConnected) {
+				setConnection(connection);
+				setProvider(connection.account);
+				setAddress(connection.selectedAddress);
+				walletStatusChange(true);
+			}
+		};
+		connectToStarknet();
+	}, []);
 
 	return (
 		<div className="main">
@@ -40,15 +85,9 @@ export default function Form() {
 				</div>
 				<div className="form-main">
 					{form === 1 ? (
-						<Deposit
-							isConnect={isConnect}
-							walletHandle={walletHandle}
-						/>
+						<Deposit isConnect={isConnect} walletHandle={walletHandle} />
 					) : (
-						<Withdraw
-							isConnect={isConnect}
-							walletHandle={walletHandle}
-						/>
+						<Withdraw isConnect={isConnect} walletHandle={walletHandle} />
 					)}
 				</div>
 			</div>
